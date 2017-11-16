a6832836c9c5e2dafa582d111f139764ebff1ad2
Mirza <mirza@github.com>
2017-11-16 20:02:49 +0100

    Again

---
+++ Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Borrowing List
+//
+//  Created by Mirza Durakovic on 16/11/2017.
+//  Copyright © 2017 Mirza Durakovic. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var name:String
+    var 
+    
+}
